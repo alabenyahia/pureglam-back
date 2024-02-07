fed43d0e432d09763a63083db9d747333810da76
Ala Ben Yahia <alabinyahia@gmail.com>
2024-02-07 15:06:07 +0100

    Add initial project structure

---
+++ src/test/java/com/pickurapps/pureglamback/PureglamBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickurapps.pureglamback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PureglamBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
